d6d6ebdea7fdd82d85028250f3b0638384567970
pedrou103 <pedroluca13.plm40@gmail.com>
2022-11-05 12:49:47 -0300

    aula 01

---
+++ mobile/App.tsx
@@ -0,0 +1,23 @@
+import { NativeBaseProvider, StatusBar} from "native-base";
+import {useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
+import { SignIn } from './src/screens/Signin';
+import { Loading } from './src/components/Loading';
+
+import { THEME } from "./src/styles/theme"
+
+export default function App() {
+
+  const [fontsloader] = useFonts({Roboto_400Regular,Roboto_500Medium,Roboto_700Bold});
+  
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      { fontsloader ? <SignIn /> : <Loading /> }
+    </NativeBaseProvider>
+  );
+}
